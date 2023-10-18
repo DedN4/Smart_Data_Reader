5a11b22f995bacf5e86b2ca2c3f25ccd975d8044
DedN4 <acslegendados16@gmail.com>
2023-10-18 12:03:07 -0300

    Adicionado funcionalidade e filtro.
    feat: finalmente consegui adicionar o filtro e o software retornou a
    funcionar corretamente, mas estamos com novos erros por agora
    
    - Erro ao trocar arquivo importado, quando importo outros dados, ele não
      descarta o anterior
    - Preciso utilizar o 'Espaço de Análise' que está em branco

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import pandas as pd
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QTextBrowser, QVBoxLayout, QWidget, QDockWidget, QToolButton, QMenu, QTableWidget, QTableWidgetItem, QAction, QMenuBar
+from PyQt5.QtWidgets import QAction, QApplication, QMainWindow, QPushButton, QFileDialog, QTextBrowser, QVBoxLayout, QWidget, QDockWidget, QTableWidget,QInputDialog, QTableWidgetItem, QMenuBar, QLabel, QComboBox
 from datetime import datetime
 
 class AnalisadorXLSX(QMainWindow):
@@ -12,15 +12,7 @@ class AnalisadorXLSX(QMainWindow):
         self.setWindowTitle("SDR - Smart Data Reader")
         self.setGeometry(100, 100, 800, 600)
 
-        # Crie um widget central para organizar a interface
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
-
-        # Layout em grade para organizar a interface
-        layout = QVBoxLayout(central_widget)
-        central_widget.setLayout(layout)
-
-        # Adicione a barra de menu
+        # Crie a barra de menu
         menubar = self.menuBar()
 
         # Crie o menu "Arquivo"
@@ -36,8 +28,16 @@ class AnalisadorXLSX(QMainWindow):
 
         # Opção para análise específica
         analise_action = QAction("Análise Específica", self)
-        analise_action.triggered.connect(self.analiseEspecifica)
+        analise_action.triggered.connect(self.iniciarAnaliseEspecifica)
         analise_menu.addAction(analise_action)
+
+        # Crie um widget central para organizar a interface
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        # Layout em grade para organizar a interface
+        layout = QVBoxLayout(central_widget)
+        central_widget.setLayout(layout)
 
         # Área para exibir estatísticas gerais
         self.textBrowser = QTextBrowser(self)
@@ -46,26 +46,19 @@ class AnalisadorXLSX(QMainWindow):
         # DataFrame original
         self.df_original = None
 
-        # Crie um botão com um menu suspenso para mostrar/ocultar os logs
-        self.log_button = QToolButton(self)
-        self.log_button.setText("Preferências")
-        self.log_button.setPopupMode(QToolButton.MenuButtonPopup)
+        # Elementos de interface do usuário para seleção
+        layout.addWidget(QLabel("Selecione a coluna principal:"))
+        self.coluna_principal_combobox = QComboBox(self)
+        layout.addWidget(self.coluna_principal_combobox)
 
-        # Crie um menu para o botão de logs
-        self.log_menu = QMenu(self.log_button)
-        self.log_button.setMenu(self.log_menu)
+        layout.addWidget(QLabel("Selecione as colunas para operações adicionais (opcional):"))
+        self.colunas_operacoes_combobox = QComboBox(self)
+        layout.addWidget(self.colunas_operacoes_combobox)
 
-        # Adicione ação para mostrar/ocultar logs
-        self.toggle_logs_action = self.log_menu.addAction("Mostrar/Esconder Logs")
-        self.toggle_logs_action.setCheckable(True)
-        self.toggle_logs_action.toggled.connect(self.toggleLogs)
-
-        # Adicione ação para mostrar/ocultar visualização de dados
-        self.toggle_visualizacao_action = self.log_menu.addAction("Mostrar/Esconder Visualização de Dados")
-        self.toggle_visualizacao_action.setCheckable(True)
-        self.toggle_visualizacao_action.toggled.connect(self.toggleVisualizacao)
-
-        layout.addWidget(self.log_button)
+        # Botão para iniciar a análise
+        self.analise_button = QPushButton("Iniciar Análise", self)
+        self.analise_button.clicked.connect(self.iniciarAnalise)
+        layout.addWidget(self.analise_button)
 
         # Crie um dock widget para os logs
         self.dock_logs = QDockWidget("Histórico", self)
@@ -74,7 +67,7 @@ class AnalisadorXLSX(QMainWindow):
         self.addDockWidget(2, self.dock_logs)
 
         self.log("Bem-vindo ao LID - Leitor Inteligente de Dados.")
-        self.log("Ações do usuário registradas:")
+        self.log("Ações do usuário registradas")
 
         # Crie um dock widget para a visualização de dados
         self.dock_visualizacao = QDockWidget("Visualização de Dados", self)
@@ -82,6 +75,28 @@ class AnalisadorXLSX(QMainWindow):
         self.dock_visualizacao.setWidget(self.visualizacao_widget)
         self.addDockWidget(1, self.dock_visualizacao)
         self.dock_visualizacao.hide()
+
+        # Crie um submenu "Preferências" em "Arquivo"
+        preferencias_submenu = arquivo_menu.addMenu("Preferências")
+
+        # Adicione uma ação para mostrar/ocultar logs
+        mostrar_logs_action = QAction("Mostrar/Esconder Logs", self)
+        mostrar_logs_action.setCheckable(True)
+        mostrar_logs_action.toggled.connect(self.toggleLogs)
+        preferencias_submenu.addAction(mostrar_logs_action)
+
+        # Crie o menu "Exibir"
+        exibir_menu = menubar.addMenu("Exibir")
+
+        # Adicione a ação para mostrar/ocultar logs
+        self.toggle_logs_action = exibir_menu.addAction("Mostrar/Esconder Logs")
+        self.toggle_logs_action.setCheckable(True)
+        self.toggle_logs_action.toggled.connect(self.toggleLogs)
+
+        # Adicione ação para mostrar/ocultar visualização de dados
+        self.toggle_visualizacao_action = exibir_menu.addAction("Mostrar/Esconder Visualização de Dados")
+        self.toggle_visualizacao_action.setCheckable(True)
+        self.toggle_visualizacao_action.toggled.connect(self.toggleVisualizacao)
 
     def log(self, entry):
         # Registre uma entrada no widget de logs
@@ -101,6 +116,11 @@ class AnalisadorXLSX(QMainWindow):
             self.addLogEntry("Arquivo carregado com sucesso.")
             self.displayDataInTable()
 
+        # Preencha os elementos de seleção de coluna com as colunas disponíveis
+        if self.df_original is not None:
+            self.coluna_principal_combobox.addItems(self.df_original.columns)
+            self.colunas_operacoes_combobox.addItems(self.df_original.columns)
+
     def addLogEntry(self, entry):
         # Adicione uma entrada ao widget de logs
         self.logs_widget.append(entry)
@@ -111,6 +131,9 @@ class AnalisadorXLSX(QMainWindow):
             self.dock_logs.show()
         else:
             self.dock_logs.hide()
+
+
+
 
     def toggleVisualizacao(self):
         # Exibe ou oculta o dock de visualização de dados com base na ação do menu
@@ -131,10 +154,41 @@ class AnalisadorXLSX(QMainWindow):
                     item = QTableWidgetItem(str(self.df_original.iloc[row, col]))
                     self.visualizacao_widget.setItem(row, col, item)
 
-    def analiseEspecifica(self):
-        # Adicione sua lógica para análise específica aqui
-        self.textBrowser.clear()
-        self.textBrowser.append("Executando análise específica...")
+    def iniciarAnaliseEspecifica(self):
+        # Abra a caixa de diálogo para selecionar a coluna principal
+        coluna, ok = QInputDialog.getItem(self, "Selecionar Coluna de Origem", "Selecione a coluna:",
+                                          self.df_original.columns, 0, False)
+
+        if ok:
+            self.log(f"Iniciou análise específica com base na coluna: {coluna}")
+            self.criarTabelaDinamica(coluna)
+
+    def iniciarAnalise(self):
+        coluna_principal = self.coluna_principal_combobox.currentText()
+        colunas_operacoes = self.colunas_operacoes_combobox.currentText()
+
+        self.log(f"Iniciou análise com base na coluna principal: {coluna_principal} e coluna(s) de operações: {colunas_operacoes}")
+        self.criarTabelaDinamica(coluna_principal, colunas_operacoes)
+
+    def criarTabelaDinamica(self, coluna_principal, colunas_operacoes=None):
+        if colunas_operacoes:
+            colunas_selecionadas = [coluna_principal] + colunas_operacoes.split(", ")
+            # Certifique-se de que as colunas selecionadas sejam únicas
+            colunas_selecionadas = list(set(colunas_selecionadas))
+        else:
+            colunas_selecionadas = [coluna_principal]
+
+        tabela_dinamica = self.df_original.groupby(colunas_selecionadas).size().reset_index(name='Quantidade')
+
+        self.visualizacao_widget.setRowCount(tabela_dinamica.shape[0])
+        self.visualizacao_widget.setColumnCount(tabela_dinamica.shape[1])
+        self.visualizacao_widget.setHorizontalHeaderLabels(tabela_dinamica.columns)
+
+        for row in range(tabela_dinamica.shape[0]):
+            for col in range(tabela_dinamica.shape[1]):
+                item = QTableWidgetItem(str(tabela_dinamica.iloc[row, col]))
+                self.visualizacao_widget.setItem(row, col, item)
+
 
 def main():
     app = QApplication(sys.argv)
