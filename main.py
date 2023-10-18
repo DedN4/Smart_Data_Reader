1e98c7ab15be32f213c514b69a858e3745719431
DedN4 <acslegendados16@gmail.com>
2023-10-18 00:31:08 -0300

    Fix: Adicionando funções
    - Função Logs em funcionamento com registro e mensagens se boas vindas
    - Preferências conta agora com funcionalidade de abrir e fechar logs
    - Correções de Erros e Intervalos ao Abrir

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QTextBrowser, QVBoxLayout, QWidget, QDockWidget, QToolButton, QMenu, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QTextBrowser, QVBoxLayout, QWidget, QDockWidget, QToolButton, QMenu, QTableWidget, QTableWidgetItem, QAction
+from datetime import datetime
 
 class AnalisadorXLSX(QMainWindow):
     def __init__(self):
@@ -31,12 +32,6 @@ class AnalisadorXLSX(QMainWindow):
         # DataFrame original
         self.df_original = None
 
-        # Crie um dock widget para os logs
-        self.dock_logs = QDockWidget("Histórico", self)
-        self.logs_widget = QTextBrowser(self)
-        self.dock_logs.setWidget(self.logs_widget)
-        self.addDockWidget(2, self.dock_logs)
-
         # Crie um botão com um menu suspenso para mostrar/ocultar os logs
         self.log_button = QToolButton(self)
         self.log_button.setText("Preferências")
@@ -51,12 +46,34 @@ class AnalisadorXLSX(QMainWindow):
         self.toggle_logs_action.setCheckable(True)
         self.toggle_logs_action.toggled.connect(self.toggleLogs)
 
+        # Adicione ação para mostrar/ocultar visualização de dados
+        self.toggle_visualizacao_action = self.log_menu.addAction("Mostrar/Esconder Visualização de Dados")
+        self.toggle_visualizacao_action.setCheckable(True)
+        self.toggle_visualizacao_action.toggled.connect(self.toggleVisualizacao)
+
         layout.addWidget(self.log_button)
 
-        # Crie uma tabela para exibir os dados da planilha
-        self.table = QTableWidget(self)
-        layout.addWidget(self.table)
-        self.table.hide()
+        # Crie um dock widget para os logs
+        self.dock_logs = QDockWidget("Histórico", self)
+        self.logs_widget = QTextBrowser(self)
+        self.dock_logs.setWidget(self.logs_widget)
+        self.addDockWidget(2, self.dock_logs)
+
+        self.log("Bem-vindo ao LID - Leitor Inteligente de Dados.")
+        self.log("Ações do usuário registradas:")
+
+        # Crie um dock widget para a visualização de dados
+        self.dock_visualizacao = QDockWidget("Visualização de Dados", self)
+        self.visualizacao_widget = QTableWidget(self)
+        self.dock_visualizacao.setWidget(self.visualizacao_widget)
+        self.addDockWidget(1, self.dock_visualizacao)
+        self.dock_visualizacao.hide()
+
+    def log(self, entry):
+        # Registre uma entrada no widget de logs
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_entry = f"[{timestamp}] {entry}"
+        self.logs_widget.append(log_entry)
 
     def abrirArquivo(self):
         options = QFileDialog.Options()
@@ -64,6 +81,7 @@ class AnalisadorXLSX(QMainWindow):
         filepath, _ = QFileDialog.getOpenFileName(self, "Abrir Arquivo XLSX", "", "Arquivos XLSX (*.xlsx);;Todos os Arquivos (*)", options=options)
 
         if filepath:
+            self.log(f"Arquivo importado: {filepath}")
             df = pd.read_excel(filepath)
             self.df_original = df
             self.addLogEntry("Arquivo carregado com sucesso.")
@@ -80,19 +98,24 @@ class AnalisadorXLSX(QMainWindow):
         else:
             self.dock_logs.hide()
 
+    def toggleVisualizacao(self):
+        # Exibe ou oculta o dock de visualização de dados com base na ação do menu
+        if self.toggle_visualizacao_action.isChecked():
+            self.dock_visualizacao.show()
+        else:
+            self.dock_visualizacao.hide()
+
     def displayDataInTable(self):
         # Exiba os dados da planilha na tabela
         if self.df_original is not None:
-            self.table.setRowCount(self.df_original.shape[0])
-            self.table.setColumnCount(self.df_original.shape[1])
-            self.table.setHorizontalHeaderLabels(self.df_original.columns)
+            self.visualizacao_widget.setRowCount(self.df_original.shape[0])
+            self.visualizacao_widget.setColumnCount(self.df_original.shape[1])
+            self.visualizacao_widget.setHorizontalHeaderLabels(self.df_original.columns)
 
             for row in range(self.df_original.shape[0]):
                 for col in range(self.df_original.shape[1]):
                     item = QTableWidgetItem(str(self.df_original.iloc[row, col]))
-                    self.table.setItem(row, col, item)
-
-            self.table.show()
+                    self.visualizacao_widget.setItem(row, col, item)
 
 def main():
     app = QApplication(sys.argv)
